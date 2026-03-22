05fb4245f2a4858fc390fc1e2cb4343c9287593b
root <root@DESKTOP-19IIAEC>
2026-03-22 02:08:15 -0500

    first commit

---
+++ seguridadNacional/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-sky-700">
+        HI, WELCOME TO THE SECURITY SYSTEM OF THE NATIONAL POLICE OF COLOMBIA
+      </h1>
+    </div>
+  )
+}
+
+export default App
